7587a7f0bccd21161f62781c1588f48a4b9c0bd7
Gus <aengus.crowley@gmail.com>
2020-01-18 23:36:05 -0800

    Pretty sure this matches requirements?

--- RighShiftTB.sv
+++ RighShiftTB.sv
@@ -1,6 +1,12 @@
+/*Write a SystemVerilog testbench model that tests your shift module by
+ applying all eight possible shift values. 
+ For each shift value apply two arbitrary data input values.
+ Choose data values that make it easy to see that the result on shout is 
+ correctly shifted. 
+*/
 'timescale 1ns/10ps
 module TwoBitRightShiftTB;
-	logic [1:0] shiftBit;
+	logic [0:2] shiftBit;
 	logic [0:7] memory;
 	logic [0:7] shout;
 
@@ -15,10 +21,22 @@ TwoBitRightShift UUT(
 //capable of going to 7 bit right shift with this implimentation
 	initial begin
 		#10 memory = 8'b10101010; shiftBit = 3'b000;
+		#10 memory = 8'b01010101; shiftBit = 3'b000;
 		#10 memory = 8'b10101010; shiftBit = 3'b001;
+		#10 memory = 8'b01010101; shiftBit = 3'b001;
 		#10 memory = 8'b10101010; shiftBit = 3'b010;
+		#10 memory = 8'b01010101; shiftBit = 3'b010;
 		#10 memory = 8'b10101010; shiftBit = 3'b011;
-		#10 $stop
+		#10 memory = 8'b01010101; shiftBit = 3'b011;
+		#10 memory = 8'b10101010; shiftBit = 3'b100;
+		#10 memory = 8'b01010101; shiftBit = 3'b100;
+		#10 memory = 8'b10101010; shiftBit = 3'b101;
+		#10 memory = 8'b01010101; shiftBit = 3'b101;
+		#10 memory = 8'b10101010; shiftBit = 3'b110;
+		#10 memory = 8'b01010101; shiftBit = 3'b110;
+		#10 memory = 8'b10101010; shiftBit = 3'b111;
+		#10 memory = 8'b01010101; shiftBit = 3'b111;
+		#10 $stop;
 	end 
 endmodule
 
