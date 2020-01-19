9ed1f1715ad5ddd87fc779af644f5a2ed614612c
Gus <aengus.crowley@gmail.com>
2020-01-18 23:01:08 -0800

    completed TB - need to ask prof abt application

---
+++ RighShiftTB.sv
@@ -0,0 +1,23 @@
+'timescale 1ns/10ps
+module TwoBitRightShiftTB;
+	logic [1:0] shiftBit;
+	logic [0:7] memory;
+	logic [0:7] shout;
+
+//initiate unit under test
+TwoBitRightShift UUT(
+	.shiftBit(shiftBit),
+	.memory   (memory),
+	.shout    (shout),
+	);
+
+// up to three bit right shift
+//capable of going to 7 bit right shift with this implimentation
+initial begin
+	#10 memory = 8'b10101010; shiftBit = 3'b000;
+	#10 memory = 8'b10101010; shiftBit = 3'b001;
+	#10 memory = 8'b10101010; shiftBit = 3'b010;
+	#10 memory = 8'b10101010; shiftBit = 3'b011;
+
+
+
