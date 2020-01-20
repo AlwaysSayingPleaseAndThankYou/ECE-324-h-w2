f3f5d59753a173a7fb99b6ec3b01784964d746ea
Gus <aengus.crowley@gmail.com>
2020-01-20 12:25:13 -0800

    currently overwritting, not shifting bits

--- RighShift.sv
+++ RighShift.sv
@@ -8,7 +8,13 @@ module TwoBitRightShift(
 	output logic [0:7] shout
 	);
 
-//shout is right shift of memory by shiftbits bits
-assign shout = memory >> shiftBit;
+
+// Shout is right shift of memory by shiftbits bits
+assign shout[0] = (shiftBit - 1 >0) ? 0 : memory[0]; // this is currently 
+assign shout[1] = (shiftBit - 2 >0) ? 0 : memory[1]; // over writting bits
+assign shout[2] = (shiftBit - 3 >0) ? 0 : memory[2]; // not shifting them
+assign shout[3] = (shiftBit - 4 >0) ? 0 : memory[3];
+assign shout[4];
+
 
 endmodule
