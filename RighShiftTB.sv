7a8daee13c3fbdc1b03ebd79fc7febcd96c930c6
Gus <aengus.crowley@gmail.com>
2020-01-21 15:26:35 -0800

    time scale has to start w a back tick fuck me

--- RighShiftTB.sv
+++ RighShiftTB.sv
@@ -4,7 +4,7 @@
  Choose data values that make it easy to see that the result on shout is 
  correctly shifted. 
 */
-'timescale 1 ns/10 ps
+`timescale 1 ns/10 ps
 module barrel_shifter;
 	logic [7:0] data;
 	logic [2:0] shift;
