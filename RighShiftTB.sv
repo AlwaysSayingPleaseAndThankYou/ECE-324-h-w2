da49ce9f639cab0d0b36a07e4b8569ee2e562d5d
Gus <aengus.crowley@gmail.com>
2020-01-21 14:53:24 -0800

    made TB work with profs code

--- RighShiftTB.sv
+++ RighShiftTB.sv
@@ -5,37 +5,37 @@
  correctly shifted. 
 */
 'timescale 1ns/10ps
-module TwoBitRightShiftTB;
-	logic [0:2] shiftBit;
-	logic [0:7] memory;
-	logic [0:7] shout;
+module barrel_shifter;
+	logic [7:0] data;
+	logic [2:0] shift;
+	logic [7:0] shout;
 
 //initiate unit under test
-TwoBitRightShift UUT(
-	.shiftBit(shiftBit),
-	.memory   (memory),
-	.shout    (shout),
+barrel_shifter UUT(
+	.data(data),
+	.shift(shift),
+	.shout(shout),
 	);
 
 // up to three bit right shift
 //capable of going to 7 bit right shift with this implimentation
 	initial begin
-		#10 memory = 8'b10101010; shiftBit = 3'b000;
-		#10 memory = 8'b01010101; shiftBit = 3'b000;
-		#10 memory = 8'b10101010; shiftBit = 3'b001;
-		#10 memory = 8'b01010101; shiftBit = 3'b001;
-		#10 memory = 8'b10101010; shiftBit = 3'b010;
-		#10 memory = 8'b01010101; shiftBit = 3'b010;
-		#10 memory = 8'b10101010; shiftBit = 3'b011;
-		#10 memory = 8'b01010101; shiftBit = 3'b011;
-		#10 memory = 8'b10101010; shiftBit = 3'b100;
-		#10 memory = 8'b01010101; shiftBit = 3'b100;
-		#10 memory = 8'b10101010; shiftBit = 3'b101;
-		#10 memory = 8'b01010101; shiftBit = 3'b101;
-		#10 memory = 8'b10101010; shiftBit = 3'b110;
-		#10 memory = 8'b01010101; shiftBit = 3'b110;
-		#10 memory = 8'b10101010; shiftBit = 3'b111;
-		#10 memory = 8'b01010101; shiftBit = 3'b111;
+		#10 data = 8'b10101010; shift = 3'b000;
+		#10 data = 8'b01010101; shift = 3'b000;
+		#10 data = 8'b10101010; shift = 3'b001;
+		#10 data = 8'b01010101; shift = 3'b001;
+		#10 data = 8'b10101010; shift = 3'b010;
+		#10 data = 8'b01010101; shift = 3'b010;
+		#10 data = 8'b10101010; shift = 3'b011;
+		#10 data = 8'b01010101; shift = 3'b011;
+		#10 data = 8'b10101010; shift = 3'b100;
+		#10 data = 8'b01010101; shift = 3'b100;
+		#10 data = 8'b10101010; shift = 3'b101;
+		#10 data = 8'b01010101; shift = 3'b101;
+		#10 data = 8'b10101010; shift = 3'b110;
+		#10 data = 8'b01010101; shift = 3'b110;
+		#10 data = 8'b10101010; shift = 3'b111;
+		#10 data = 8'b01010101; shift = 3'b111;
 		#10 $stop;
 	end 
 endmodule
