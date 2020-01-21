e6af0a42ccf60230487565ab5345656acd9d21b9
Gus <aengus.crowley@gmail.com>
2020-01-20 23:59:43 -0800

    comparator works properly

--- RighShift.sv
+++ RighShift.sv
@@ -11,17 +11,17 @@ module TwoBitRightShift(
 
 
 // Shout is right shift of memory by shiftbits bits
-assign shout[0] = (shiftBit - 1 >0) ? 0 : memory[7 + shiftBit]; 
-assign shout[1] = (shiftBit - 2 >0) ? 0 : memory[6 + shiftBit]; 
-assign shout[2] = (shiftBit - 3 >0) ? 0 : memory[5 + shiftBit]; 
-assign shout[3] = (shiftBit - 4 >0) ? 0 : memory[4 + shiftBit];  
+assign shout[0] = (shiftBit - 1 >=0) ? 0 : memory[7 + shiftBit]; 
+assign shout[1] = (shiftBit - 2 >=0) ? 0 : memory[6 + shiftBit]; 
+assign shout[2] = (shiftBit - 3 >=0) ? 0 : memory[5 + shiftBit]; 
+assign shout[3] = (shiftBit - 4 >=0) ? 0 : memory[4 + shiftBit];  
 // TODO: reverse endian!
 // TODO: solve wraparound at end? This prolly involves another ternary
 // should be the bit several places above it? 
-assign shout[4] = (shiftBit - 5 >0) ? 0 : memory[3 + shiftBit];
-assign shout[5] = (shiftBit - 6 >0) ? 0 : memory[2 + shiftBit];
-assign shout[6] = (shiftBit - 7 >0) ? 0 : memory[1 + shiftBit];
-assign shout[7] = (shiftBit - 8 >0) ? 0 : memory[0 + shiftBit];
+assign shout[4] = (shiftBit - 5 >=0) ? 0 : memory[3 + shiftBit];
+assign shout[5] = (shiftBit - 6 >=0) ? 0 : memory[2 + shiftBit];
+assign shout[6] = (shiftBit - 7 >=0) ? 0 : memory[1 + shiftBit];
+assign shout[7] = (shiftBit - 8 >=0) ? 0 : memory[0 + shiftBit];
 
 
 endmodule
